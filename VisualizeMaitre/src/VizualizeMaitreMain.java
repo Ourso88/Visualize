fdfa103ece8cec2faa2f1350dc9c50066b77b6c1
Eric Mariani <eric.axiome@gmail.com>
2016-12-06 19:18:56 +0100

    EM : Premier commit

---
+++ VisualizeMaitre/src/VizualizeMaitreMain.java
@@ -0,0 +1,15 @@
+
+
+/**
+ * Programme main
+ * 		* Lancement du programme 
+ * @author Eric Mariani
+ * @since 06/12/2016
+ *
+ */
+public class VizualizeMaitreMain {
+	public static void main(String[] args) {
+		System.out.println("Lancement du programme maitre");
+	}
+
+}
